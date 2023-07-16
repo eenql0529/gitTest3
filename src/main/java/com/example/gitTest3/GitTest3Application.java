833e4ca7767b0aca86ae1babf49dfc02cc779556
eenql0529 <eenql0529@daum.net>
2023-07-16 21:39:48 +0900

    “메모”

---
+++ src/main/java/com/example/gitTest3/GitTest3Application.java
@@ -0,0 +1,13 @@
+package com.example.gitTest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTest3Application.class, args);
+	}
+
+}
